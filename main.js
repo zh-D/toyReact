092caded9121c8b08b4cdc6d1dec821f935e94b1
zh-D <1641245614@qq.com>
2020-07-22 15:13:35 +0800

    第二天

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
 import { ToyReact, Component } from './ToyReact.js'
 
-class Myconponent extends Component {
-    render() {
-        return <div>
-            <span>Hello</span>
-            <span>World</span>
-            <div>
-                {this.children}
-            </div>
-        </div>
+class Square extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value:null
+        }
     }
-
+    render() {
+        return (
+            <button className='square' onClick={()=>this.setState({value:'x'})}>
+             {this.state.value ? this.state.value : ''}
+             </button>
+        )
+    }
 }
 
-let a = <Myconponent name='a' id='ida'>
-    <div>123123</div>
-</Myconponent>
+class Board extends Component {
+    renderSquare(i) {
+        return <Square value={i} />
+    }
+    render() {
+        return (
+            <div>
+                <div className='board-row'>
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className='board-row'>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+let a = <Board />
+
+
+
 
 ToyReact.render(
     a,
@@ -27,3 +58,19 @@ ToyReact.render(
 
 
 
+// class Myconponent extends Component {
+//     render() {
+//         return <div>
+//             <span>Hello</span>
+//             <span>World</span>
+//             <div>
+//                 {this.children}
+//             </div>
+//         </div>
+//     }
+
+// }
+
+// let a = <Myconponent name='a' id='ida'>
+//     <div>123123</div>
+// </Myconponent>
